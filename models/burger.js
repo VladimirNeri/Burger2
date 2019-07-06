867e755c0e3ebb609c8adf85f0d2f2af3dda5b1e
obscura101 <vlad.neri@gmail.com>
2019-07-05 22:19:20 -0700

    Final

--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Burger = sequelize.define('Burger', {
     burger_name: DataTypes.STRING,
-    devoured: DataTypes.INTEGER
+    devoured: DataTypes.BOOLEAN
   }, {});
   Burger.associate = function(models) {
     // associations can be defined here
