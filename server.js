1c9c1ebfccba2c236f07a4092c8087d105f92027
Vlad N <vlad.neri@gmail.com>
2019-07-06 08:37:06 -0700

    Final

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ require("./controllers/burgers_controller.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: false}).then(function() {
+db.sequelize.sync({ force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
